90e5c1d135a4f51b1646248addceef7c700d8116
CatherinRevu <catherin.catherin362@gmail.com>
2021-05-05 15:46:24 +0530

    this is the third change

---
+++ ForLoopTest.java
@@ -0,0 +1,17 @@
+//Multiplication Table
+import java.util.Scanner;
+public class ForLoopTest {
+
+    public static void main(String[] args) {
+Scanner myObj = new Scanner(System.in);
+System.out.println("Enter number");
+       int num = myObj.nextInt();
+System.out.println("now printing multiplication table of"+num);
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+System.out.println("table complete");
+System.out.println("-----------------------");
+    }
+}
