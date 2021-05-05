9bd2f13c54f84edd63e86fe822dbe77158d78f13
CatherinRevu <51873439+CatherinRevu@users.noreply.github.com>
2021-05-05 13:05:28 +0530

    Add files via upload
    added 2 lines

--- ForLoopTest.java
+++ ForLoopTest.java
@@ -6,9 +6,11 @@ public class ForLoopTest {
 Scanner myObj = new Scanner(System.in);
 System.out.println("Enter number");
        int num = myObj.nextInt();
+System.out.println("now printing multiplication table of"+num);
         for(int i = 1; i <= 10; ++i)
         {
             System.out.printf("%d * %d = %d \n", num, i, num * i);
         }
+System.out.println("table complete");
     }
 }
